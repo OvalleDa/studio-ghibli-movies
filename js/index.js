524f7ba36ae5d4c3ae79352cc0eb5be452225332
Daniela Ovalle <daniovape@hotmail.com>
2022-05-20 19:45:55 -0500

    project studio ghibli

---
+++ js/index.js
@@ -0,0 +1,70 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.personaje1', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-60px'
+});
+
+sr.reveal('.button-info',  {
+    duration: 6000,
+    origin: 'bottom',
+    distance: '460px'
+});
+
+sr.reveal('.personaje2',  {
+    duration: 3000,
+    origin: 'top',
+    interval: 106,
+    distance: '100px'
+});
+
+sr.reveal('.personaje3',  {
+    duration: 3000,
+    origin: 'top',
+    interval: 106,
+    distance: '100px'
+});
+
+sr.reveal('.personaje4',  {
+    duration: 3000,
+    origin: 'top',
+    interval: 106,
+    distance: '100px'
+});
+
+sr.reveal('.personaje5',  {
+    duration: 3000,
+    origin: 'top',
+    interval: 106,
+    distance: '100px'
+});
+
+
+sr.reveal('.container-info',  {
+    duration: 3000,
+    origin: 'rigth',
+    interval: 106,
+    distance: '100px'
+});
+
+sr.reveal('.container-info2',  {
+    duration: 3000,
+    origin: 'rigth',
+    interval: 106,
+    distance: '100px'
+});
+
+sr.reveal('.container-info3',  {
+    duration: 3000,
+    origin: 'rigth',
+    interval: 106,
+    distance: '100px'
+});
+
+sr.reveal('.container-info4',  {
+    duration: 3000,
+    origin: 'rigth',
+    interval: 106,
+    distance: '100px'
+});
